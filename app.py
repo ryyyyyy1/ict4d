3a801d8cd8432af7d29b58942517addb65bae745
ryyyyyy1 <rongyi7015@gmail.com>
2024-04-23 00:10:33 +0800

    test heroku v1

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hello ict4d!"
+
+@app.route('/test')
+def test():
+    return "this is a test"
+
+if __name__ == '__main__':
+    app.run()
