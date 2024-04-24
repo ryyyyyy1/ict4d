fd359ec17d8e1c624f3d1b7971db26da87225302
Ifon <1565975426@qq.com>
2024-04-24 21:24:43 +0200

    redirect to search result

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,24 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    print('searching...')
+    if request.method == "GET":
+        print(request)
+        cert_id = request.args.get('cert_id')
+        print(cert_id)
+    else:
+        print('unhandled method')
+    return redirect(url_for('search_result'))
+
+
+@app.route('/search_result')
+def search_result():
+    # 渲染搜索结果页面
+    return render_template('certification.html')
 
 
 @app.route("/test")
